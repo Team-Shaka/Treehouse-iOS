ec9948db10bf91a344da4ab8dfb0f1c204e9db9d
BoMin <lbmleebm@gmail.com>
2024-02-17 18:17:07 +0900

    FEAT: Add MainTabBarViewController

--- Treehouse/Scenes/MainTabBarViewController.swift
+++ Treehouse/Scenes/MainTabBarViewController.swift
@@ -1,6 +1,6 @@
 //
 //  MainTabBarViewController.swift
-//  Tree
+//  Treehouse
 //
 //  Created by BoMin Lee on 2/13/24.
 //
@@ -11,19 +11,32 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .trBackgroundWhite
+        
         let feedViewController = FeedViewController()
-//        let treeViewController =
-//        let notificationViewController =
-//        let settingViewController =
+        let branchViewController = BranchViewController()
+        let notificationViewController = NotificationViewController()
+        let settingViewController = SettingViewController()
         
-//        feedViewController.tabBarItem.image =
+        feedViewController.tabBarItem.image = TreehouseImageCollection.tabBarIconFeedNormal.withRenderingMode(.alwaysOriginal)
+        feedViewController.tabBarItem.selectedImage = TreehouseImageCollection.tabBarIconFeedSelected.withRenderingMode(.alwaysOriginal)
+        branchViewController.tabBarItem.image = TreehouseImageCollection.tabBarIconBranchNormal.withRenderingMode(.alwaysOriginal)
+        branchViewController.tabBarItem.selectedImage = TreehouseImageCollection.tabBarIconBranchSelected.withRenderingMode(.alwaysOriginal)
+        notificationViewController.tabBarItem.image = TreehouseImageCollection.tabBarIconNotificationNormal.withRenderingMode(.alwaysOriginal)
+        notificationViewController.tabBarItem.selectedImage = TreehouseImageCollection.tabBarIconNotificationSelected.withRenderingMode(.alwaysOriginal)
+        settingViewController.tabBarItem.image = TreehouseImageCollection.tabBarIconSettingNormal.withRenderingMode(.alwaysOriginal)
+        settingViewController.tabBarItem.selectedImage = TreehouseImageCollection.tabBarIconSettingSelected.withRenderingMode(.alwaysOriginal)
         
-        feedViewController.navigationItem.largeTitleDisplayMode = .never
+//        feedViewController.navigationItem.largeTitleDisplayMode = .never
         
         let navigationFeed = UINavigationController(rootViewController: feedViewController)
+        let navigationBranch = UINavigationController(rootViewController: branchViewController)
+        let navigationNotification = UINavigationController(rootViewController: notificationViewController)
+        let navigationSetting = UINavigationController(rootViewController: settingViewController)
         
-        navigationFeed.navigationBar.prefersLargeTitles = false
+//        navigationFeed.navigationBar.prefersLargeTitles = false
+//        navigationBranch.navigationBar.prefersLargeTitles = false
         
-        setViewControllers([navigationFeed], animated: false)
+        setViewControllers([navigationFeed, navigationBranch, navigationNotification, navigationSetting], animated: false)
     }
 }
