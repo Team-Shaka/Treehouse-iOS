4c3d36e9382d85e3baa2c60072b53f510fe27657
BoMin <lbmleebm@gmail.com>
2024-02-17 18:16:52 +0900

    FEAT: Add SettingViewController

---
+++ Treehouse/Scenes/SettingScenes/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  Treehouse
+//
+//  Created by BoMin Lee on 2/13/24.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        addSubviews()
+        makeConstraint()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func makeConstraint() {
+        
+    }
+}
