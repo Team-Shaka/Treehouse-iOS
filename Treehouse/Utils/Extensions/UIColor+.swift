d98653558ee87912abe9c612b55d6b646a433638
BoMin <lbmleebm@gmail.com>
2024-02-17 18:16:24 +0900

    CHORE: Edit backgroundWhite color

--- Treehouse/Utils/Extensions/UIColor+.swift
+++ Treehouse/Utils/Extensions/UIColor+.swift
@@ -10,5 +10,5 @@ import UIKit
 extension UIColor {
     static let trPrimaryGreen: UIColor = UIColor(red: 92 / 255, green: 144 / 255, blue: 84 / 255, alpha: 1)
     static let trPrimaryGray: UIColor = UIColor(red: 156 / 255, green: 156 / 255, blue: 156 / 255, alpha: 1)
-    static let trBackgroundWhite: UIColor = UIColor(red: 249 / 255, green: 248 / 255, blue: 246 / 255, alpha: 1)
+    static let trBackgroundWhite: UIColor = UIColor(red: 255 / 255, green: 255 / 255, blue: 255 / 255, alpha: 1)
 }
