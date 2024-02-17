2d002d2f2618288032ad0bf9d0d6e45bb6e1eb13
BoMin <lbmleebm@gmail.com>
2024-02-17 18:16:36 +0900

    CHORE: Add images to collection

---
+++ Treehouse/Utils/TreehouseCollections/TreehouseImageCollection.swift
@@ -0,0 +1,23 @@
+//
+//  TreehouseImageCollection.swift
+//  Treehouse
+//
+//  Created by BoMin Lee on 2/13/24.
+//
+
+import UIKit
+
+final class TreehouseImageCollection {
+    private init() { }
+    
+    static let tabBarIconFeedNormal: UIImage = #imageLiteral(resourceName: "tabBarIcon_feed_normal")
+    static let tabBarIconFeedSelected: UIImage = #imageLiteral(resourceName: "tabBarIcon_feed_selected")
+    static let tabBarIconBranchNormal: UIImage = #imageLiteral(resourceName: "tabBarIcon_branch_normal")
+    static let tabBarIconBranchSelected: UIImage = #imageLiteral(resourceName: "tabBarIcon_branch_selected")
+    static let tabBarIconNotificationNormal: UIImage = #imageLiteral(resourceName: "tabBarIcon_notification_normal")
+    static let tabBarIconNotificationSelected: UIImage = #imageLiteral(resourceName: "tabBarIcon_notification_selected")
+    static let tabBarIconSettingNormal: UIImage = #imageLiteral(resourceName: "tabBarIcon_setting_normal")
+    static let tabBarIconSettingSelected: UIImage = #imageLiteral(resourceName: "tabBarIcon_setting_selected")
+    
+    static let changeTreehouse: UIImage = #imageLiteral(resourceName: "changeTreehouse")
+}
