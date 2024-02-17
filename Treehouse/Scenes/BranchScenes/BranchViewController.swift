33f95b24ab2f09fd484b3d249d81d3b8a37a6eea
BoMin <lbmleebm@gmail.com>
2024-02-17 18:17:35 +0900

    FEAT: Add BranchViewController

---
+++ Treehouse/Scenes/BranchScenes/BranchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BranchViewController.swift
+//  Treehouse
+//
+//  Created by BoMin Lee on 2/13/24.
+//
+
+import UIKit
+
+class BranchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        addSubviews()
+        makeConstraint()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func makeConstraint() {
+        
+    }
+}
