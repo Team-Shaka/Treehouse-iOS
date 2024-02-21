6e1fae8ca9424b56ecd599db0931b7f25326d089
BoMin <lbmleebm@gmail.com>
2024-02-22 01:59:19 +0900

    CHORE: Edit UIColor+

--- Treehouse/Utils/Extensions/UIColor+.swift
+++ Treehouse/Utils/Extensions/UIColor+.swift
@@ -10,5 +10,9 @@ import UIKit
 extension UIColor {
     static let trPrimaryGreen: UIColor = UIColor(red: 92 / 255, green: 144 / 255, blue: 84 / 255, alpha: 1)
     static let trPrimaryGray: UIColor = UIColor(red: 156 / 255, green: 156 / 255, blue: 156 / 255, alpha: 1)
+    static let trTextGray: UIColor = UIColor(red: 232 / 255, green: 233 / 255, blue: 233 / 255, alpha: 1)
     static let trBackgroundWhite: UIColor = UIColor(red: 255 / 255, green: 255 / 255, blue: 255 / 255, alpha: 1)
+    
+    static let trHallGradientGreen: UIColor = UIColor(red: 0.482, green: 0.725, blue: 0.439, alpha: 1)
+    static let trHallGradientBlue: UIColor = UIColor(red: 0.408, green: 0.702, blue: 0.976, alpha: 1)
 }
