0d417c583ad03e1c40fca24269d6bbb2551d8804
BoMin <lbmleebm@gmail.com>
2024-03-05 15:26:21 +0900

    CHORE: Add substring

--- Treehouse/Utils/Extensions/String+.swift
+++ Treehouse/Utils/Extensions/String+.swift
@@ -24,4 +24,15 @@ extension String {
         dateFormatter.timeZone = .current
         return dateFormatter.date(from: self)
     }
+    
+    func substring(from: Int, to: Int) -> String {
+        guard from < count, to >= 0, to - from >= 0 else {
+            return ""
+        }
+        
+        let startIndex = index(self.startIndex, offsetBy: from)
+        let endIndex = index(self.startIndex, offsetBy: to + 1)
+        
+        return String(self[startIndex ..< endIndex])
+    }
 }
