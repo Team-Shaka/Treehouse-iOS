3225604ec39f572e71326f29e414be7d807b2b6e
BoMin <lbmleebm@gmail.com>
2024-03-05 15:39:31 +0900

    FEAT: Add NotificationScenes

---
+++ Treehouse/Scenes/NotificationScenes/Cells/NotificationTableViewCell.swift
@@ -0,0 +1,122 @@
+//
+//  NotificationTableViewCell.swift
+//  Treehouse
+//
+//  Created by BoMin Lee on 3/2/24.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+    static let identifier: String = String(describing: NotificationTableViewCell.self)
+    
+    var notification: NotificationData! {
+        didSet {
+            update()
+        }
+    }
+    
+    private var mainContainerView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private var userImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private var contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 16)
+        label.textColor = .black
+        label.text = "아럼닝러ㅗㅁㅇㄹㅇ님이 좋아용를 ㅇ라ㅓㄴ마ㅣㅇ러ㅐㅁ냐륟"
+        return label
+    }()
+    
+    private var timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 12)
+        label.textColor = .trSubTextGray
+        label.text = "30분 전"
+        return label
+    }()
+    
+    private var separatorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 12)
+        label.textColor = .trSubTextGray
+        label.text = "·"
+        return label
+    }()
+    
+    private var treehouseNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 12)
+        label.textColor = .trSubTextGray
+        label.text = "점심팟"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+        addSubviews()
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        self.backgroundColor = .clear
+        self.contentView.backgroundColor = .clear
+    }
+    
+    func addSubviews() {
+        self.contentView.addSubview(mainContainerView)
+        
+        mainContainerView.addSubviews(userImageView,
+                                      contentLabel,
+                                      timeLabel,
+                                      separatorLabel,
+                                      treehouseNameLabel)
+    }
+    
+    func makeConstraints() {
+        mainContainerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        userImageView.snp.makeConstraints{ make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(18)
+            make.height.width.equalTo(36)
+        }
+        
+        contentLabel.snp.makeConstraints{ make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(userImageView.snp.trailing).offset(14)
+            make.trailing.equalToSuperview().inset(16)
+        }
+        
+    }
+    
+    func update() {
+//        self.contentLabel.text = notification.message
+    }
+}
+
+extension NotificationTableViewCell {
+    static func makeCell(_ tableView: UITableView,
+                         notification: NotificationData) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationTableViewCell.identifier) as? NotificationTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        cell.notification = notification
+        return cell
+    }
+}
--- Treehouse/Scenes/NotificationScenes/NotificationViewController.swift
+++ Treehouse/Scenes/NotificationScenes/NotificationViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class NotificationViewController: UIViewController {
+    private var notifications: [NotificationData] = []
+    
+    private var topView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private var notificationLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 30, weight: .bold)
+        label.text = "알림"
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private var notificationTableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
@@ -15,15 +37,64 @@ class NotificationViewController: UIViewController {
         makeConstraint()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
     private func configure() {
+        notificationTableView.delegate = self
+        notificationTableView.dataSource = self
         
+        notificationTableView.register(NotificationTableViewCell.self, forCellReuseIdentifier: NotificationTableViewCell.identifier)
     }
     
     private func addSubviews() {
+        view.addSubviews(topView, notificationTableView)
         
+        topView.addSubview(notificationLabel)
     }
     
     private func makeConstraint() {
+        topView.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(60)
+        }
         
+        notificationLabel.snp.makeConstraints{ make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(16)
+        }
+        
+        notificationTableView.snp.makeConstraints{ make in
+            make.top.equalTo(topView.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func fetchNotifcation() {
+        TreehouseNetworkManager.shared.fetchNotification { [weak self] value, error in
+            guard let self = self else { return }
+//            print(value, error)
+            self.notifications = value ?? []
+            self.notificationTableView.reloadData()
+        }
+    }
+}
+
+extension NotificationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.notifications.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let notification = notifications[safe: indexPath.row] else { return UITableViewCell() }
+        let cell = NotificationTableViewCell.makeCell(tableView,
+                                                      notification: notification)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
